b0af4f6407aa66d1b4fde3a6c59ec194627c85dd
Anass Benfillous <anasse.benfillous@gmail.com>
2025-01-31 12:53:25 +0100

    update album crud

--- backend/target/generated-sources/annotations/io/benfill/isdb/mapper/UserMapperImpl.java
+++ backend/target/generated-sources/annotations/io/benfill/isdb/mapper/UserMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-01-30T05:52:51+0100",
+    date = "2025-01-31T12:51:36+0100",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.4 (Eclipse Adoptium)"
 )
 @Component
